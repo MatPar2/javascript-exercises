86a60808c9303f519dfb4bf9ff5ef47c4bf4b83d
MatPar <paradelamatias@gmail.com>
2022-07-11 16:57:43 -0300

    Make a loop to sum values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,24 @@
 const sumAll = function(initialNumber, finalNumber) {
+  // Check if any number is negative, or if it's not an integer, and return an error message
   if (initialNumber < 0 || finalNumber < 0 || initialNumber !== parseInt(initialNumber) || finalNumber !== parseInt(finalNumber)) {
     return "ERROR";
   }
-};
 
-console.log(sumAll(-10, 4));
+  // Order values from smallest to largest
+  if (initialNumber > finalNumber) {
+    let tmp = initialNumber;
+    initialNumber = finalNumber;
+    finalNumber = tmp;
+  }
+
+  let counter = 0;
+  // For-loop option
+  for (let i = initialNumber; i <= finalNumber; i++) {
+    counter += i;
+  }
+
+  return counter;
+};
 
 // Do not edit below this line
 module.exports = sumAll;
