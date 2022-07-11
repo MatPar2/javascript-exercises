fd406cecf2b7909992d564c4eb61ec8c065bc5ca
MatPar <paradelamatias@gmail.com>
2022-07-11 12:52:21 -0300

    Solve 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
 const repeatString = function(str, num) {
-  return 
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  let finalStr = '';
+
+  for (let i = 1; i <= num; i++) {
+    finalStr += str;
+  }
+
+  return finalStr;
 };
 
 // Do not edit below this line
