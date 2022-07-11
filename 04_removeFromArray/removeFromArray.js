6a129f377b31fac2a5248c326b06343d4b5a43c0
MatPar <paradelamatias@gmail.com>
2022-07-11 13:00:48 -0300

    Solve 4th exercise. removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  let arr = args[0];
 
+  for (let i = 1; i < args.length; i++) {
+    arr = arr.filter((elem) => {
+      if (elem !== args[i]) {
+        return elem;
+      }
+    });
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
