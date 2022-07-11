a1e27747370a6f0e4c9584430235160e57903399
MatPar <paradelamatias@gmail.com>
2022-07-11 17:20:08 -0300

    Set function conversion from celcius to farenheite

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -4,8 +4,10 @@ const ftoc = function(temp) {
   return parseFloat(result.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let result = temp * 1.8 + 32;
+  
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
