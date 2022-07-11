4413a8bc705e37aa8c8f9e7b5c88bc1e9a65d8e6
MatPar <paradelamatias@gmail.com>
2022-07-11 13:03:57 -0300

    Style changes seeing the solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,10 @@
-const repeatString = function(str, num) {
-  if (num < 0) {
-    return "ERROR";
-  }
+const repeatString = function(word, times) {
+  if (times < 0) return "ERROR";
 
   let finalStr = '';
 
-  for (let i = 1; i <= num; i++) {
-    finalStr += str;
+  for (let i = 1; i <= times; i++) {
+    finalStr += word;
   }
 
   return finalStr;
