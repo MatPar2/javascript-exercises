cd555a46055961fb1c4dc9aa013bd4532c5aa71a
MatPar <paradelamatias@gmail.com>
2022-07-13 20:03:03 -0300

    Solve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const rawLetters = str.toLowerCase().replace(/[\s,\.!]/g, "");
+  return rawLetters.split('').reverse().join('') === rawLetters;
 };
+
+console.log(palindromes("Racecar!"));
 
 // Do not edit below this line
 module.exports = palindromes;
