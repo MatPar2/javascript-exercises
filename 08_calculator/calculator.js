5b3cd8b5ec32ad6a645f184ce830ee86332ba33e
MatPar <paradelamatias@gmail.com>
2022-07-13 20:29:04 -0300

    Minus changes watching the solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,14 +7,12 @@ const subtract = function(a,b) {
 };
 
 const sum = function(arr) {
-	return arr.reduce((sum, elem) => {
-    return sum += elem;
-  }, 0);
+	return arr.reduce((sum, elem) => sum + elem, 0);
 };
 
 const multiply = function(arr) {
   return arr.reduce((mult, elem) => {
-    return mult *= elem;
+    return mult * elem;
   }, 1);
 };
 
@@ -24,7 +22,7 @@ const power = function(a, b) {
 
 const factorial = function(num) {
 	if (num === 0 || num === 1) return 1;
-  if (num < 0) return "ERROR. Negative number";
+  if (num < 0) return "ERROR. Negative number";
 
   return factorial(num-1) * num;
 };
