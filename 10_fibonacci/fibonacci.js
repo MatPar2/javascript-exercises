2bfbea9e7dec0f4eaa5a9850d2b407cf9be0b31f
MatPar <paradelamatias@gmail.com>
2022-07-13 20:31:33 -0300

    Minus changes watching the solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
 const fibonacci = function(indexNum) {
   if (indexNum < 0) return "OOPS";
+  if (indexNum === 0) return 0;
   let fibonacciSeq = [1, 1];
   for (let i = 1; i < indexNum; i++) {
     fibonacciSeq.push(fibonacciSeq[i] + fibonacciSeq[i-1]);
