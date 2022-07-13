3196dfda95c5a8c47a153fde265b8eca47b7bce3
MatPar <paradelamatias@gmail.com>
2022-07-13 20:11:27 -0300

    Solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(indexNum) {
+  if (indexNum < 0) return "OOPS";
+  let fibonacciSeq = [1, 1];
+  for (let i = 1; i < indexNum; i++) {
+    fibonacciSeq.push(fibonacciSeq[i] + fibonacciSeq[i-1]);
+  }
 
+  return fibonacciSeq[indexNum-1];
 };
 
 // Do not edit below this line
