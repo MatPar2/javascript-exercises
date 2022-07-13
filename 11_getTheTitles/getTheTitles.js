478c6f17d5b980f7ef05825777057d3a66a757aa
MatPar <paradelamatias@gmail.com>
2022-07-13 20:16:05 -0300

    Solve reduce exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksArr) {
+  return booksArr.reduce((arr, elem) => {
+    return arr.concat(elem.title);
+  }, []);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
