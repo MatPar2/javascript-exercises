0f524a2722bc28c2cbc8a0e6f01a3b24fe332749
MatPar <paradelamatias@gmail.com>
2022-07-13 20:35:03 -0300

    Clean up

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -12,23 +12,5 @@ const findTheOldest = function(peopleArr) {
   }, {});
 };
 
-const people = [
-  {
-    name: "Carly",
-    yearOfBirth: 1066,
-  },
-  {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-  },
-  {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-  },
-]
-console.log(findTheOldest(people));
-
 // Do not edit below this line
 module.exports = findTheOldest;
