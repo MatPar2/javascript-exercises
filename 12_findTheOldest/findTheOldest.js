45bbdcc00e1235495f8f20fb5f79688598aaf184
MatPar <paradelamatias@gmail.com>
2022-07-13 20:24:14 -0300

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArr) {
+  return peopleArr.reduce((obj, elem) => {
+    if (!elem.yearOfDeath) {
+      elem.yearOfDeath = new Date().getFullYear();
+    }
+    const age1 = elem.yearOfDeath - elem.yearOfBirth;
+    const age2 = obj.yearOfDeath - obj.yearOfBirth;
+    if (age1 < age2) {
+      return obj;
+    } 
+    return elem;
+  }, {});
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
